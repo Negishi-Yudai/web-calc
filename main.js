48faf66b0798f9482f2a930129d42f94aae8cdaf
negishi <negi4028@gmail.com>
2020-10-18 03:36:58 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const buttonAdd = document.getElementById('button-add');
+const buttonSub = document.getElementById('button-sub');
+const buttonMul = document.getElementById('button-mul');
+const buttonDiv = document.getElementById('button-div');
+
+const box = document.getElementById('box');
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+
+const getNum1 = () => {
+  // num1の数値を戻り値としてreturnする処理を書いてください
+  const numberNum1 = Number.parseFloat(num1.value);
+  return numberNum1;
+};
+
+const getNum2 = () => {
+  // num2の数値を戻り値としてreturnする処理を書いてください
+  const numberNum2 = Number.parseFloat(num2.value);
+  return numberNum2;
+};
+
+const showResult = (num) => {
+  // <div id="box">にnumを表示する処理を書いてください
+  box.textContent = num;
+};
+
+buttonAdd.addEventListener('click', () => {
+  const result = getNum1() + getNum2();
+  showResult(result);
+});
+
+buttonSub.addEventListener('click', () => {
+  
+  const result = getNum1() - getNum2();
+  showResult(result);
+});
+
+buttonMul.addEventListener('click', () => {
+  
+  const result = getNum1() * getNum2();
+  showResult(result);
+});
+
+buttonDiv.addEventListener('click', () => {
+  
+  const result = getNum1() / getNum2();
+  showResult(result);
+});
